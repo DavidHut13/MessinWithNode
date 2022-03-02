6e1653a9bd89b671693863df70da69ff15753fce
David Hutto <INDH4799@innovallc.com>
2022-03-01 21:38:04 -0600

    :tada Initial Commit:

---
+++ app.js
@@ -0,0 +1,16 @@
+const { appendFile } = require('fs');
+const {mongoConnect} = require('./database');
+const express = require('express');
+const getDb = require('./database').getDb;
+ const app = express()
+
+
+mongoConnect(() => {
+    console.log('Connected')
+    app.listen(3000)
+});
+
+
+// const db = getDb();
+// db.collection('TestData').InsertOne({Event: 'Test Game', id: 1234})
+
